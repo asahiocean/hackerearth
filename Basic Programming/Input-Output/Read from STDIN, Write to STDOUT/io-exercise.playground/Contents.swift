d763445e083a8349d48cf7eb7798dd82d0f159ea
asahiocean <61920574+asahiocean@users.noreply.github.com>
2022-02-17 04:55:19 +0500

    Update: Read from STDIN, Write to STDOUT
    Co-Authored-By: sergfedotov <96548632+sergfedotov@users.noreply.github.com>

--- Basic Programming/Input-Output/Read from STDIN, Write to STDOUT/io-exercise.playground/Contents.swift
+++ Basic Programming/Input-Output/Read from STDIN, Write to STDOUT/io-exercise.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // Read from STDIN, Write to STDOUT
-// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/io-exercise
+// https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/io-exercise/
 
 if let N = Int(readLine(strippingNewline: true)!), Set(0...10).contains(N) { print(N * 2) }
 
